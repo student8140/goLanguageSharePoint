6258ebfc42c74813a022f9ae7ec5025d930a4a6c
student8140 <timepass.321123@gmail.com>
2024-07-01 17:34:18 +0530

    Update sharepointAccess.go

--- sharepointAccess.go
+++ sharepointAccess.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "bytes"
+    "encoding/base64"
     "encoding/json"
     "fmt"
     "io/ioutil"
@@ -11,116 +11,107 @@ import (
 )
 
 const (
-    tenantID     = "YOUR_TENANT_ID"
-    clientID     = "YOUR_CLIENT_ID"
-    clientSecret = "YOUR_CLIENT_SECRET"
-    siteID       = "YOUR_SITE_ID"
-    listID       = "YOUR_LIST_ID"
+    tenantID       = "YOUR_TENANT_ID"
+    clientID       = "YOUR_CLIENT_ID"
+    clientSecret   = "YOUR_CLIENT_SECRET"
+    resource       = "https://graph.microsoft.com"
+    scope          = "https://graph.microsoft.com/.default"
+    authorityHost  = "https://login.microsoftonline.com"
+    siteID         = "YOUR_SITE_ID"
 )
 
 func getAccessToken() (string, error) {
-    form := url.Values{}
-    form.Add("grant_type", "client_credentials")
-    form.Add("client_id", clientID)
-    form.Add("client_secret", clientSecret)
-    form.Add("resource", "https://graph.microsoft.com")
+    // Construct the request URL
+    tokenURL := fmt.Sprintf("%s/%s/oauth2/v2.0/token", authorityHost, tenantID)
 
-    resp, err := http.PostForm(fmt.Sprintf("https://login.microsoftonline.com/%s/oauth2/token", tenantID), form)
+    // Create the request payload
+    data := url.Values{}
+    data.Set("client_id", clientID)
+    data.Set("client_secret", clientSecret)
+    data.Set("grant_type", "client_credentials")
+    data.Set("scope", scope)
+
+    // Create a new HTTP request
+    req, err := http.NewRequest("POST", tokenURL, strings.NewReader(data.Encode()))
     if err != nil {
         return "", err
     }
-    defer resp.Body.Close()
+    req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        return "", err
-    }
-
-    var result map[string]interface{}
-    if err := json.Unmarshal(body, &result); err != nil {
-        return "", err
-    }
-
-    return result["access_token"].(string), nil
-}
-
-func validateTokenClaims(accessToken string) (bool, error) {
-    parts := strings.Split(accessToken, ".")
-    if len(parts) != 3 {
-        return false, fmt.Errorf("invalid token format")
-    }
-
-    payload, err := base64.RawURLEncoding.DecodeString(parts[1])
-    if err != nil {
-        return false, err
-    }
-
-    var claims map[string]interface{}
-    if err := json.Unmarshal(payload, &claims); err != nil {
-        return false, err
-    }
-
-    if scp, ok := claims["scp"]; ok && scp != "" {
-        return true, nil
-    }
-    if roles, ok := claims["roles"]; ok && len(roles.([]interface{})) > 0 {
-        return true, nil
-    }
-
-    return false, fmt.Errorf("neither scp nor roles claim is present in the token")
-}
-
-func getSharePointList(accessToken string) error {
+    // Send the request
     client := &http.Client{}
-    req, err := http.NewRequest("GET", fmt.Sprintf("https://graph.microsoft.com/v1.0/sites/%s/lists/%s/items", siteID, listID), nil)
-    if err != nil {
-        return err
-    }
-
-    req.Header.Set("Authorization", "Bearer "+accessToken)
-    req.Header.Set("Accept", "application/json")
-
     resp, err := client.Do(req)
     if err != nil {
-        return err
+        return "", err
     }
     defer resp.Body.Close()
 
+    // Read the response body
     body, err := ioutil.ReadAll(resp.Body)
     if err != nil {
-        return err
+        return "", err
     }
 
-    var result map[string]interface{}
-    if err := json.Unmarshal(body, &result); err != nil {
-        return err
+    // Parse the JSON response
+    var tokenResp map[string]interface{}
+    if err := json.Unmarshal(body, &tokenResp); err != nil {
+        return "", err
     }
 
-    // Process the list items
-    fmt.Println(result)
+    // Extract the access token
+    accessToken := tokenResp["access_token"].(string)
+    return accessToken, nil
+}
 
-    return nil
+func validateTokenClaims(accessToken string) (map[string]interface{}, error) {
+    parts := strings.Split(accessToken, ".")
+    if len(parts) != 3 {
+        return nil, fmt.Errorf("invalid token format")
+    }
+
+    // Decode the payload
+    payload, err := base64.RawURLEncoding.DecodeString(parts[1])
+    if err != nil {
+        return nil, err
+    }
+
+    // Unmarshal JSON
+    var claims map[string]interface{}
+    if err := json.Unmarshal(payload, &claims); err != nil {
+        return nil, err
+    }
+
+    return claims, nil
 }
 
 func main() {
+    // Get access token
     accessToken, err := getAccessToken()
     if err != nil {
         fmt.Println("Error getting access token:", err)
         return
     }
 
-    isValid, err := validateTokenClaims(accessToken)
+    // Validate token claims
+    claims, err := validateTokenClaims(accessToken)
     if err != nil {
         fmt.Println("Error validating token claims:", err)
         return
     }
 
-    if !isValid {
-        fmt.Println("Token does not contain required claims")
-        return
+    // Print scp and roles claims
+    scpClaim, scpExists := claims["scp"]
+    rolesClaim, rolesExists := claims["roles"]
+
+    if scpExists {
+        fmt.Println("scp:", scpClaim)
+    } else {
+        fmt.Println("scp claim is not present")
     }
 
-    if err := getSharePointList(accessToken); err != nil {
-        fmt.Println("Error getting SharePoint list:", err)
+    if rolesExists {
+        fmt.Println("roles:", rolesClaim)
+    } else {
+        fmt.Println("roles claim is not present")
     }
 }
