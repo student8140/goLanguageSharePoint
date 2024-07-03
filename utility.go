88b337d0b2603e125724af085c342d15d16b8e54
student8140 <timepass.321123@gmail.com>
2024-07-03 16:13:14 +0530

    Update utility.go

--- utility.go
+++ utility.go
@@ -1,1 +1,4 @@
 https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/authorize?client_id={client_id}&response_type=code&redirect_uri=https://localhost&response_mode=query&scope=https://graph.microsoft.com/.default&state=12345
+
+
+Content-Type: application/x-www-form-urlencoded
