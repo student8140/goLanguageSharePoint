3caff1fac77bc89b2698da5b8b9e69680c29e694
student8140 <timepass.321123@gmail.com>
2024-07-03 16:07:38 +0530

    Create utility.go

---
+++ utility.go
@@ -0,0 +1,1 @@
+https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/authorize?client_id={client_id}&response_type=code&redirect_uri=https://localhost&response_mode=query&scope=https://graph.microsoft.com/.default&state=12345
