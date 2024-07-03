ac2947a00dbca718e82786b636a429f09b7257f2
student8140 <timepass.321123@gmail.com>
2024-07-03 16:13:55 +0530

    Update utility.go

--- utility.go
+++ utility.go
@@ -2,3 +2,11 @@ https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/authorize?client_id={c
 
 
 Content-Type: application/x-www-form-urlencoded
+
+
+grant_type: authorization_code
+client_id: {client_id} (your application/client ID from Azure AD)
+client_secret: {client_secret} (the client secret you generated)
+code: {authorization_code} (the authorization code you just obtained)
+redirect_uri: https://localhost (or your specified redirect URI)
+scope: https://graph.microsoft.com/.default
